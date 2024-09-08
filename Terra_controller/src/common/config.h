a22bebd449b35911d048086c9a9bc8331bed177f
Anh Phan <pbt.anh19992@gmail.com>
2024-09-08 11:29:08 +0700

    change waterFlow pin to pin 3

--- Terra_controller/src/common/config.h
+++ Terra_controller/src/common/config.h
@@ -5,19 +5,18 @@
 
 // Sensor Pins
 const int SOIL_MOISTURE_PIN = A0;
+const int WATER_DETECTION_PIN = 3;
 const int DHT_SENSOR_PIN = 2;
 
 // Relay Control Pins
-const int RELAY_PELTIER_PIN = 6;
-const int RELAY_LIGHT_PIN = 5;
-const int RELAY_PUMP_PIN = 4;
-const int RELAY_VALVE_PIN = 3;
+const int RELAY_PELTIER_PIN = 7;
+const int RELAY_LIGHT_PIN = 6;
+const int RELAY_PUMP_PIN = 5;
+const int RELAY_VALVE_PIN = 4;
 
 // RGB LED PWM Pins
 const int LED_RED_PIN = 11;
 const int LED_GREEN_PIN = 10;
 const int LED_BLUE_PIN = 9;
-
-const int WATER_DETECTION_PIN = 7;
 
 #endif // CONFIG_H
