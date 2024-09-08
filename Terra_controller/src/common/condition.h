041577a0634361d7ed491ac7035ee2ce4f736156
Anh Phan <pbt.anh19992@gmail.com>
2024-09-08 11:10:54 +0700

    add control condition

---
+++ Terra_controller/src/common/condition.h
@@ -0,0 +1,28 @@
+#ifndef CONDITION_H
+#define CONDITION_H
+
+enum ConditionType {
+    HUMIDITY,
+    TEMPERATURE,
+    WATER,
+    TIME_BASED
+};
+
+/**!
+ * @struct Condition
+ * @brief Represents a conditon interface to control a relay
+ * 
+ * This `struct` provides variables to control a relay in different condition
+*/
+struct Condition
+{
+    ConditionType type;
+    float humidThreshold;
+    float temperatureThreshold;
+    float moistureThreshold;
+    int duration; //Minutes
+    int startTime; //24hour HHMM format
+};
+
+
+#endif //CONDITON_H
